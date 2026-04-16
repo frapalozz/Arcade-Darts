4ee6891168ed6fab1f50567b8df95237ca2cdff1
Francesco Palozzi <99217899+frapalozz@users.noreply.github.com>
2026-04-16 12:24:50 +0200

    add home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,20 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Press_Start_2P, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const spaceGrotesk = Space_Grotesk({
+  variable: "--font-space-grotesk",
+  subsets: ['latin'],
+  weight: ["300", "400", "500", "600", "700"],
+  display: "swap"
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const pressStart = Press_Start_2P({
+  variable: "--font-press-start",
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['400']
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +29,11 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${pressStart.variable} ${spaceGrotesk.variable} font-headline h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full w-full flex flex-col bg-black">
+        {children}
+      </body>
     </html>
   );
 }
