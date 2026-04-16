83e1f7582ee3aeb44fb8d932a72857c41814460c
Francesco Palozzi <99217899+frapalozz@users.noreply.github.com>
2026-04-16 12:36:50 +0200

    fixed home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,32 @@
+"use client"
 import { Rocket, Unplug } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [roomId, setRoomId] = useState('');
+  const [lobbyId, setLobbyId] = useState('');
+  const router = useRouter();
+
+  const createGame = () => {
+    if(roomId.length === 0 ) return;
+    const id = roomId.trim();
+    router.push(`/game/${id}`);
+  }
+
+  const joinGame = () => {
+    if(lobbyId.length === 0 ) return;
+    const id = lobbyId.trim();
+    router.push(`/game/${id}`);
+  }
+
+  const spectator = () => {
+    if(lobbyId.length === 0 ) return;
+    const id = lobbyId.trim();
+    router.push(`/game/${id}?mode=spectator`);
+  }
+
   return (
     <div className="">
       <main className="flex flex-col items-center justify-center min-h-screen gap-5">
@@ -19,8 +45,15 @@ export default function Home() {
               <Rocket className="stroke-primary" /> 
               <p className="text-xs uppercase">Nuova_Partita</p>
             </div>
-            <p className={`text-neutral-400 text-xs font-medium font-body`}>Crea una nuova lobby e invita giocatori a partecipare</p>
-            <button className="bg-primary text-on-primary text-xs p-3 w-full mt-auto">Crea</button>
+            <p className={`text-primary-dim text-[0.45rem] uppercase`}>Crea una nuova lobby e invita giocatori a partecipare</p>
+            <input 
+              type="text" 
+              className="bg-neutral-800 outline outline-primary text-primary px-2 text-sm" 
+              placeholder="nome lobby"
+              value={roomId}
+              onChange={e => setRoomId(e.target.value)}
+            />
+            <button onClick={createGame} className="cursor-pointer bg-primary text-on-secondary text-xs p-3 w-full mt-auto">Crea</button>
           </div>
 
           <div className="h-full w-full border border-secondary-container p-4 gap-2 max-w-75 flex flex-col">
@@ -29,8 +62,15 @@ export default function Home() {
               <p className="text-xs uppercase">Partecipa</p>
             </div>
             <p className={`text-[0.45rem] text-secondary-dim uppercase`}>enter_lobby_code</p>
-            <input type="text" className="bg-neutral-800 outline outline-secondary text-secondary px-2 text-sm" />
-            <button className="bg-secondary text-on-secondary text-xs p-3 w-full mt-auto">Partecipa</button>
+            <input 
+              type="text" 
+              className="bg-neutral-800 outline outline-secondary text-secondary px-2 text-sm" 
+              placeholder="codice lobby"
+              value={lobbyId}
+              onChange={e => setLobbyId(e.target.value)}
+            />
+            <button onClick={joinGame} className="cursor-pointer bg-secondary text-on-secondary text-xs p-3 w-full mt-auto">Partecipa</button>
+            <button onClick={spectator} className="cursor-pointer bg-secondary-container text-on-secondary text-xs p-1 w-full mt-auto">Spettatore</button>
           </div>
         </div>
         
